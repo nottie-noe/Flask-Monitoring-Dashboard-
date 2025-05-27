a8a85527a6e40022da2a32ed51ec9e220b403cbf
nottie-noe <noelee.ndlovu@gmail.com>
2025-05-27 13:31:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def dashboard():
@@ -20,6 +22,10 @@ def dashboard():
         </body>
     </html>
     '''
+
+@app.route('/hello')
+def hello():
+    return "Hello from Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
